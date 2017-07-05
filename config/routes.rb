a9e23db0d7f34fc76d49fb575367d723a02f0113
realtimschmidt <schmidt.tim.j@gmail.com>
2017-07-05 15:30:29 -0500

    curl reqeust works

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
-    resources :users, only: [:new, :create]
+    resources :users
   end
 
   get 'about' => 'welcome#about'
