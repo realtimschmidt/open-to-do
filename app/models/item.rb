9e0e6d56522a7ce90d1a5dd196179fca1c2cfbf1
realtimschmidt <schmidt.tim.j@gmail.com>
2017-07-05 17:08:43 -0500

    Create from command line complete

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,6 @@
 class Item < ActiveRecord::Base
   belongs_to :list
+
+  validates :item_title, length: { minimum: 1, maximum: 100 }, presence: true
+  validates :description, presence: true, length: { minimum: 1 }
 end
--- app/models/list.rb
+++ app/models/list.rb
@@ -1,4 +1,6 @@
 class List < ActiveRecord::Base
   belongs_to :user
   has_many :items
+
+  validates :list_title, length: { minimum: 1, maximum: 100 }, presence: true
 end
